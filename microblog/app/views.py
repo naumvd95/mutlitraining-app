5d6d025c482ec93d2d15dd50a980ce6abcb065f1
naumvd95 <naumvd95@gmail.com>
2017-03-24 16:33:13 +0300

    progress 3.5/18

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -1,6 +1,7 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
 from datetime import date
+from forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -24,3 +25,13 @@ def index():
         user = user,
         posts = posts)
 
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html',
+        title = 'Sign In',
+        form = form,
+        providers = app.config['OPENID_PROVIDERS'])
